f5712ca7d9bf709339e69be21ce15a2a14ce9e82
Fabiojunio12 <fabiojuniodeamaral@gmail.com>
2022-04-18 21:16:41 -0300

    quarto commit

---
+++ exercicios/ex053.py
@@ -0,0 +1,12 @@
+#crie um programa que leia um frase qualquer e diga se ela é um palíndromo, desconsiderando os espaços.
+frase = str(input('Digite um frase: ')).strip().upper()
+palavras = frase.split()
+junto = ''.join(palavras)
+inverso = junto[::-1]
+'''for letra in range(len(junto) - 1, - 1, - 1):
+	inverso += junto[letra]'''
+print('O inverso de {} {}'.format(junto, inverso))
+if inverso == junto:
+	print('Temos um palíndromo.')
+else:
+	print('A frase digitada não é um palídromo.')
